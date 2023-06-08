1f6384116cfed72e09c9a5435b9402a1a52469cd
salihhkck <salihkucukdeveloper@gmail.com>
2023-06-09 01:51:04 +0300

    cshtml pages are edited. Services added BusinessLayer. And more.

---
+++ DataAccessLayer/Abstract/IGuideDal.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGuideDal:IGenericDal<Guide>
+    {
+       
+    }
+}
---
+++ TraversalCoreProject/Controllers/LayoutPartialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class LayoutPartialController : Controller
+    {
+        public PartialViewResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
