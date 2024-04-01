45de97a72a9db0f4fad6f7fa96dd601cd765f944
Salih Kucuk <salihkucukdeveloper@gmail.com>
2024-04-01 05:27:39 +0300

    The project index page was designed with partialviews. While concretes were defined on the Business Layer side for new views, partials were created under components.

---
+++ BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        ITestimonialDal _testimonial;
+
+        public TestimonialManager(ITestimonialDal testimonial)
+        {
+            _testimonial = testimonial;
+        }
+
+        public void TAdd(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Testimonial TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Testimonial> TGetList()
+        {
+            return _testimonial.GetList();
+        }
+
+        public void TUpdate(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_PopularDestinationPartial.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+
+namespace TraversalCoreProject.Views.Default
+{
+    public class _PopularDestinationPartial: ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values=destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
