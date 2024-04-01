09235efa59c574f371421ad433cf9cdebcdbd6fe
Salih Kucuk <salihkucukdeveloper@gmail.com>
2024-04-01 14:10:22 +0300

    UI additions have been made.

---
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        public IActionResult DestinationDetails(Destination d)
+        {
+            return View(d);
+        }
+    }
+}
